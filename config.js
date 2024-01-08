b96555b54ed50bea221623fccbe25ea4a0271ee7
MohdDaniish <70433418+MohdDaniish@users.noreply.github.com>
2024-01-08 07:26:19 +0000

    node email registration setup

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    databaseURL: 'mongodb+srv://danishekaasha:AGDipo825nihRXol@cluster0.mongodb.net/Businesses?retryWrites=true&w=majority',
+    secret: 'your_jwt_secret',
+  };
